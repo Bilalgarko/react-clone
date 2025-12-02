88adcdf66b11650a901743fac7c0f83edb313d4f
Garko_Jr <bilalgarko77@gmail.com>
2025-12-02 22:13:31 +0000

    Add footer component and update navbar styles
    Introduced a new Footer component with associated styles and integrated it into the App layout. Updated navbar CSS for improved layout and appearance. Added a logo image to the public images directory.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/shared/Navbar";  
+import Footer from "./components/shared/Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Footer />
     </div>
   );
 }
